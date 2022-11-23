69480ff606137f92d7d545ae0b4bd1f31cb5e94b
Evalyne <evalyne.mueke@student.moringaschool.com>
2022-11-23 22:49:02 +0300

    Done with Assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+function divide(){
+    return 2000/100;
+}
+//arrow function called square that has one parameter a number an multiply it by itself
+const square =(number) =>number*number;
+
+//arrow function called add that has 2 parameters and adds them
+const add = (num1,num2) => num1  + num2;
